8db94a7fc2c250174a1411ca36e9b562ec4275cd
Vladimir Drozdeckiy <mrgreyves@gmail.com>
2024-02-08 23:21:07 +0300

    feature toggle have been added

--- redis/redis.go
+++ redis/redis.go
@@ -1,15 +1,26 @@
 package redis
 
 import (
-	"github.com/labstack/echo/v4"
 	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
 
 func Getfromredis(c echo.Context) error {
-	return c.String(http.StatusOK, "OK")
+	if os.Getenv("REDIS_ENABLED") == "true" {
+		return c.String(http.StatusOK, "OK")
+	} else {
+		return c.String(http.StatusOK, "Redis is not enabled")
+	}
+
 }
 
 func Puttoredis(c echo.Context) error {
-	return c.String(http.StatusOK, "OK")
+	if os.Getenv("REDIS_ENABLED") == "true" {
+		return c.String(http.StatusOK, "OK")
+	} else {
+		return c.String(http.StatusOK, "Redis is not enabled")
+	}
 }
